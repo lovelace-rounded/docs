44dbd7b928f270a5eaae33acf2501b7d7297604f
acesyde <acesyde@gmail.com>
2023-03-11 14:14:37 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# 🟣 Rounded documentation
+
+## Installation
+
+* Open the project with vscode and devcontainer
+
+## Commands
+
+
+### Dev
+
+```bash
+yarn run dev
+```
+
+### Build
+
+```bash
+yarn run build
+```
+
+### Start
+
+```bash
+yarn run start
+```
---
+++ theme.config.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>🟣 Lovelace Rounded</span>,
+  project: {
+    link: 'https://github.com/lovelace-rounded/ui',
+  },
+  docsRepositoryBase: 'https://github.com/lovelace-rounded/lovelace-rounded.github.io',
+  footer: {
+    text: 'Lovelace Rounded Documentation',
+  },
+}
+
+export default config
